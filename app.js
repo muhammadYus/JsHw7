8b951c38a5db815c8a376510f3ff40269106119e
muhammadYus <74594824+muhammadYus@users.noreply.github.com>
2022-01-20 18:06:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let output = document.getElementById("output");
+let a = {
+    apple:100,
+    samsung:200,
+    Alish: true,
+    I:"Her",
+    cyberatack: null,
+    underfined: 1,
+    react: false,
+    F: "protothype",
+    N: "",
+    r:0,   
+};
+const {apple,samsung,Alish,I,cyberatack,underfined,react,F,N,r} = a
+function b(result){
+    if(a != Number){
+        console.log(false);
+    }
+    result = apple,samsung,Alish,I,cyberatack,underfined,react,F,N,r;
+    for({apple,samsung,Alish,I,cyberatack,underfined,react,F,N,r} = a ; a = result; a*2){
+        output.innerHTML = a;
+    }
+}
+ b = output.innerHTML+= "<br>"
